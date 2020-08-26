7f8fd32dd1546be5c86bf0231b57a59ce7f16165
Shaun Brown <shaun.brown@mail.house.gov>
2020-08-26 09:30:08 -0700

    9ebfe72

---
+++ _nuxt/pages/index.443516f.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{288:function(e,n,t){"use strict";t.r(n);var c={name:"index"},r=t(45),component=Object(r.a)(c,(function(){var e=this.$createElement;return(this._self._c||e)("div")}),[],!1,null,"8db0badc",null);n.default=component.exports}}]);
